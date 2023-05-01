3bc7ae21f9406212874670ef5492a613b7878ead
sb23 <soumyabubai23@gmail.com>
2023-05-02 01:34:50 +0530

    Adding CRUD Operations

--- db/migrate/20230429172307_add_timestamps_to_articles.rb
+++ db/migrate/20230429172307_add_timestamps_to_articles.rb
@@ -1,6 +1,5 @@
 class AddTimestampsToArticles < ActiveRecord::Migration[5.1]
   def change
-              #table  field_name field_type
     add_column :articles, :created_at, :datetime
     add_column :articles, :updated_at, :datetime
   end
